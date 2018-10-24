ba973c6c0c814fdc96ebee2d0ba1fd61039e60e1
hubergalviz <ha.galviz@uniandes.edu.co>
2018-10-23 22:25:25 -0500

    check

--- SGRED_HG/SGRED_HG/settings.py
+++ SGRED_HG/SGRED_HG/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'SGRED_HG.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': "SGRED",
-    #     'USER': "Willy",
-    #     "PASSWORD": "",
-    #     "HOST": "127.0.0.1",
-    #     "PORT": "5432"
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "sgred",
-        'USER': "root",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "3306"
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': "SGRED",
+         'USER': "postgres",
+         "PASSWORD": "123",
+         "HOST": "127.0.0.1",
+         "PORT": "5432"
+     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': "sgred",
+    #    'USER': "root",
+    #    "PASSWORD": "root",
+    #    "HOST": "127.0.0.1",
+    #    "PORT": "3306"
+    #}
 }
 
 
