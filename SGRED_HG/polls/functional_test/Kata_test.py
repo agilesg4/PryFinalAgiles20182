92989ae6a03638fc88932f72b6bf7c575e024fa2
Jorge Martín <je.martin@uniandes.edu.co>
2018-11-01 00:54:14 -0500

    [Verde] Se crea código para que se muestren los detalles principales de una actividad.

--- SGRED_HG/polls/functional_test/Kata_test.py
+++ SGRED_HG/polls/functional_test/Kata_test.py
@@ -15,15 +15,18 @@ class Kata_test(TestCase):
 
     def test_1_title(self):
         self.browser.get('http://127.0.0.1:8000/')
+        self.browser.implicitly_wait(3)
         self.assertIn('SGRED', self.browser.title)
 
     def test_detalle_actividad(self):
-        self.browser.get('http://127.0.0.1:8000/listar_actividades/1/')
+        self.browser.get('http://127.0.0.1:8000/polls/listar_actividades/1/')
+        self.browser.implicitly_wait(3)
         titulo = self.browser.find_element_by_id("tituloactividad")
         self.assertIn("Actividad 1", titulo.text)
 
     def test_servicio_detalle_actividad(self):
         self.browser.get('http://127.0.0.1:8000/rest_actividades_id/1')
+        self.browser.implicitly_wait(3)
         texto = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 1\"')]")
         self.assertIn("Actividad 1", texto.text)
         try:
@@ -33,18 +36,19 @@ class Kata_test(TestCase):
         self.assertNotIn("Actividad 2", texto2)
 
     def test_detalle_actividad_2(self):
-        self.browser.get('http://127.0.0.1:8000/listar_actividades/1/')
+        self.browser.get('http://127.0.0.1:8000/polls/listar_actividades/1/')
+        self.browser.implicitly_wait(3)
         nombre = self.browser.find_element_by_id("tituloactividad")
         self.assertIn("Actividad 1", nombre.text)
         descripcion = self.browser.find_element_by_id("descripcion")
         self.assertIn("Descripcion Actividad 1", descripcion.text)
         tipoact = self.browser.find_element_by_id("tipoactividad")
-        self.assertNotIn(None, tipoact.text)
+        self.assertFalse(tipoact is None)
         idfase = self.browser.find_element_by_id("fase")
-        self.assertNotIn(None, idfase.text)
+        self.assertFalse(idfase is None)
         periodicidad = self.browser.find_element_by_id("periodicidad")
         self.assertIn("Diario", periodicidad.text)
         fecha_inicio = self.browser.find_element_by_id("fechainicio")
-        self.assertNotIn(None, fecha_inicio)
+        self.assertFalse(fecha_inicio is None)
 
 
