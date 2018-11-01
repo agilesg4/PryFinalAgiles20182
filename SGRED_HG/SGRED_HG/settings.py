0c8e2d9ff1f3f29062955d6692bb9eb511aeaa28
jc-mojicap <jc.mojicap@uniandes.edu.co>
2018-11-01 00:15:37 -0500

    Ajuste DB

--- SGRED_HG/SGRED_HG/settings.py
+++ SGRED_HG/SGRED_HG/settings.py
@@ -79,15 +79,16 @@ WSGI_APPLICATION = 'SGRED_HG.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "sgred",
-        'USER': "root",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "3306"
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': "SGRED",
+         'USER': "root",
+         "PASSWORD": "",
+         "HOST": "127.0.0.1",
+         "PORT": "1111"
+     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
