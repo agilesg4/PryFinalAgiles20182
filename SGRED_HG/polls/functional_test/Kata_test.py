68fffbfd0f7b345e31b063f600f1ad1c8d607996
Jorge Martín <je.martin@uniandes.edu.co>
2018-10-31 23:58:32 -0500

    [Rojo] Se refactoriza la prueba

--- SGRED_HG/polls/functional_test/Kata_test.py
+++ SGRED_HG/polls/functional_test/Kata_test.py
@@ -23,8 +23,11 @@ class Kata_test(TestCase):
         self.assertIn("Actividad 1", titulo.text)
 
     def test_servicio_detalle_actividad(self):
-        self.browser.get('http://127.0.0.1:8000/rest_actividades_id')
+        self.browser.get('http://127.0.0.1:8000/rest_actividades_id/1')
         texto = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 1\"')]")
         self.assertIn("Actividad 1", texto.text)
-        texto = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 2\"')]")
-        self.assertNotIn("Actividad 2", texto.text)
+        try:
+            texto2 = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 2\"')]")
+        except:
+            texto2 = "prueba"
+        self.assertNotIn("Actividad 2", texto2)
