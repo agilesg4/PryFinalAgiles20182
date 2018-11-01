82f857026fbc83f0c15f45ed0157f642aada6c01
Jorge Martín <je.martin@uniandes.edu.co>
2018-10-31 23:29:57 -0500

    [Rojo] Se crea  prueba para probar que el servicio de detalle de actividad encuentre el registro actividad 1 y no encuentre otro registro. deben existir 2 registro en la base de datos con nombres "Actividad 1" y "Actividad 2"

--- SGRED_HG/polls/functional_test/Kata_test.py
+++ SGRED_HG/polls/functional_test/Kata_test.py
@@ -21,3 +21,10 @@ class Kata_test(TestCase):
         self.browser.get('http://127.0.0.1:8000/listar_actividades/1/')
         titulo = self.browser.find_element_by_id("tituloactividad")
         self.assertIn("Actividad 1", titulo.text)
+
+    def test_servicio_detalle_actividad(self):
+        self.browser.get('http://127.0.0.1:8000/rest_actividades_id')
+        texto = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 1\"')]")
+        self.assertIn("Actividad 1", texto.text)
+        texto = self.browser.find_element_by_xpath("//*[contains(text(), '\"nombre\": \"Actividad 2\"')]")
+        self.assertNotIn("Actividad 2", texto.text)
