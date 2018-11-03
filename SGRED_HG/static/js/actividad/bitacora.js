63e53d5e70e59b589e9f67d352e704267d49fc13
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-11-02 22:32:38 -0500

    intento de arreglar la redreccion a los endpoints

--- SGRED_HG/static/js/actividad/bitacora.js
+++ SGRED_HG/static/js/actividad/bitacora.js
@@ -19,28 +19,10 @@ $(document).ready(function() {
                     contentType: false,
                     processData: false,
                     success: function () {
-                        //window.location = "/polls/recurso/";
+                        window.location = "/polls/lista_actividades/";
                     },
 
                 });
                 console.log(formData)
                 });
 
-function mySubmitFunction(e)
-{
-  e.preventDefault();
-   var formData = new FormData($("#formbitacora")[0]);
-
-        $.ajax({
-            url: $(this).attr('action'),
-            type: $(this).attr('method'),
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function () {
-                window.location = "/polls/bitacora/";
-            },
-        });
-        console.log(formData)
- }
